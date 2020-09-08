c29740c4b51534ba566b1df852cbf9440d2e8f59
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-09-09 00:10:13 +0900

    Add files via upload

--- hopage2/script.js
+++ hopage2/script.js
@@ -1,13 +1,2 @@
-const response=document.querySelector('p');
-var check=0;
-response.addEventListener('mouseover',function()
-{
-    check=1;
-    console.log(check);
-});
-response.addEventListener('mouseout',function()
-{
-    check=0;
-    console.log(check);
-});
+
 
