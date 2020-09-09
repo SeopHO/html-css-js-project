ba7d3579911e8248f2299cd555197576da71dfff
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-09-10 00:09:41 +0900

    Add files via upload

--- hopage2/script.js
+++ hopage2/script.js
@@ -1,2 +1,26 @@
+let btn_plus = document.querySelector('.click-bar p');
+let trans_image = document.querySelector('.bg-image');
+ 
+let check=0;
 
+btn_plus.addEventListener('click',function(event)
+{
+    trans_image.style.height='50vh';
+});
 
+btn_plus.addEventListener('mouseover',function(event)
+{
+    check=1;
+    btn_plus.classList.toggle('trans_plus_1');
+    console.log(check);
+    
+    
+});
+
+btn_plus.addEventListener('mouseout',function(event)
+{
+    check=0;
+    btn_plus.classList.toggle('trans_plus_2');
+    console.log(check);
+
+});
