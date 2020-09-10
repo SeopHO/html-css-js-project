84d1936dd6a97829493d9dbe414b0e72ad285b5f
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-09-10 23:35:22 +0900

    Add files via upload

--- hopage2/script.js
+++ hopage2/script.js
@@ -1,26 +1,41 @@
-let btn_plus = document.querySelector('.click-bar p');
-let trans_image = document.querySelector('.bg-image');
- 
-let check=0;
-
-btn_plus.addEventListener('click',function(event)
+function imageSlide()
 {
-    trans_image.style.height='50vh';
-});
+    let bar_plus = document.querySelector('.click-bar');
+    let btn_plus = document.querySelector('.click-bar p');
+    let trans_image = document.querySelector('.bg-image');
+     
+    let ckeck_click=true;
 
-btn_plus.addEventListener('mouseover',function(event)
-{
-    check=1;
-    btn_plus.classList.toggle('trans_plus_1');
-    console.log(check);
+    btn_plus.addEventListener('click',function(event)
+    {
+        if(ckeck_click === true)
+        {
+            // trans_image.style.height='50vh';
+            trans_image.classList.toggle('trans-image-off');
+            bar_plus.classList.toggle('bar-trans-off');
+            ckeck_click=false;
+        }
+        else if(ckeck_click === false)
+        {
+            // trans_image.style.height='100vh';
+            trans_image.classList.toggle('trans-image-on');
+            bar_plus.classList.toggle('bar-trans-on');
+            ckeck_click=true;
+        }
+    });
     
+    // btn_plus.addEventListener('mouseover',function(event)
+    // {
+    //     let check=1;
+    //     btn_plus.classList.add('trans_plus_1');
+
+    // });
     
-});
+    // btn_plus.addEventListener('mouseout',function(event)
+    // {
+    //     let check=0;
+    //     btn_plus.classList.add('trans_plus_2');
 
-btn_plus.addEventListener('mouseout',function(event)
-{
-    check=0;
-    btn_plus.classList.toggle('trans_plus_2');
-    console.log(check);
-
-});
+    // }); 
+}
+imageSlide();
